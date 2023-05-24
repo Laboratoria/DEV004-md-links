ae8b10850d2070ab7f5d42f3ffd69b589569cec5
Carolina Paredes Arias <carolina.paredes.arias@gmail.com>
2023-05-24 00:43:41 -0400

    se verifica si la ruta existe

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const mdLinks = (path, options) => {
-  return new Promise((resolve, reject) => (
+const fs = require('fs');
 
-  ));
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) =>{ //creamos una nueva promesa
+//verificamos si la ruta existe
+if (fs.existsSync(path)) {
+
+}else {
+//si no existe la ruta, se rechaza la promesa
+reject('La ruta no existe');
 }
 
-module.exports = () => {
+});
+};
 
+module.exports = {
+  mdLinks
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../index.js');
+const {mdLinks} = require('../index.js');
 
 
 describe('mdLinks', () => {
@@ -6,8 +6,13 @@ describe('mdLinks', () => {
   it('should...', () => {
     console.log('FIX ME!');
   });
-  it('Debería devolver una promesa', () => {
-    expect(mdLinks()).toBe(typeof Promise);
+  //it('Debería devolver una promesa', () => {
+    //expect(mdLinks()).toBe(typeof Promise);
+  //});
+  it('Debería rechazar cuando el path no existe', () =>{
+    return mdLinks('/carolina/mirror/noexiste.md').catch((error)=>{
+expect(error).toBe("La ruta no existe");
+    })
 
   });
 });
