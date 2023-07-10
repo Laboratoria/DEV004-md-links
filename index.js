d10cfe545141edbe2345f86caf7453ff6f1de163
Fernanda Acuña <fdafilgueira@gmail.com>
2023-07-10 11:56:30 -0400

    comentarios añadidos

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
 
-// Aqui se importan fs, path, etc ---funcion
-// export const mdLinks = (path, options) => {
+// Aqui estan las funciones pequeñas que se exportan a mdlinks.js
+
 // const fs = require('fs'); --no vamos a usar el require.
 import fs from 'fs'
 import path from 'path';
+//ruta es el readme.md
 // funcion que dice si la ruta existe o no
 export const mdExists = (ruta) => {
-    if (fs.existsSync(ruta)){
-        return true
-        }else{
-        return false
-        }
-/*const ruta = fs.stat(path, (err, stats) => {
-    if (err) {
-        if (err.code === 'ENOENT') {
-            console.error('Path does not exist');
-        } else {
-            console.error('An error occurred while checking the path:', err);
-        }
-    } else {
-        console.log('Path exists');
-    }
-});*/
+  // podemos resumir la funcion retornando solo el fs.ES
+  if (fs.existsSync(ruta)) {
+    return true
+  } else {
+    return false
+  }
+
+
+
+  /*const ruta = fs.stat(path, (err, stats) => {
+      if (err) {
+          if (err.code === 'ENOENT') {
+              console.error('Path does not exist');
+          } else {
+              console.error('An error occurred while checking the path:', err);
+          }
+      } else {
+          console.log('Path exists');
+      }
+  });*/
 };
 
 // esta funcion verifica si el archivo en la ruta dada es valido
 // y devuelve una promesa
-export const mdValid = (ruta) =>{
+export const mdValid = (ruta) => {
   //se crea una promesa con los parametros de callback resolve y reject
-  return new Promise((resolve, reject)=>{
-//la funcion asincronica fs.stat toma la ruta del archivo y una funcion callback
+  return new Promise((resolve, reject) => {
+    //la funcion asincronica fs.stat toma la ruta del archivo y una funcion callback
+    //esta funcion tmb se puede resumir un poco
     fs.stat(ruta, (err, stats) => {
       //throw error dentiene la ejecucion y se podria manejar en otro lugar de mi codigo
       if (err) throw err;
       // console.log(stats.isFile());
       // stats.isFile verifica si el archivo es un archivo valido, si da true: continua con la verificacion
-      if (stats.isFile()== true) {
+      if (stats.isFile() == true) {
         //con path.extname se obtiene la extencion del archivo, si es md, se resuelve true, sino reject
-          if(path.extname(ruta) == '.md'){
-              resolve(true)
-          }else{
-            reject(false)
-          }
+        if (path.extname(ruta) == '.md') {
+          resolve(true)
+        } else {
+          reject(false)
+        }
 
-      }else{
-          console.log('No soportamos');
+      } else {
+        console.log('No soportamos');
       }
-  });
+    });
   })
-    
-      
+
+
 }
 
 // revisar promesa y callback
@@ -58,6 +64,15 @@ export const mdValid = (ruta) =>{
 // googlear 'leer el contenido d eun archivo node js
 // intentar implementar dentro de mdRead
 
-export const mdRead = ()=>{
+export const mdRead = (ruta) => {
+  //la funcion readfile lee el archivo ruta, utf-8 es para leer en modo texto
+  //err y data son callbacks, debe retornar una promesa (new promise)//
+  fs.readFile(ruta, 'utf-8', (err, data) => {
+    if (err) {
+      console.log('no leyo el archivo: ', err);
+    } else {
+      console.log('si leyo ', data);
+    }
+  });
 
 }
--- mdlinks.js
+++ mdlinks.js
@@ -6,7 +6,7 @@ import path from 'path'
 // import { mdLinks } from "./mdlinks";
 
 // se importan estas funciones desde index.js
-import { mdExists, mdValid } from './index.js'
+import { mdExists, mdValid, mdRead } from './index.js'
 
 const ruta = "README.md";  // existe
 //const ruta = "REDME.md";  // NO existe
@@ -18,8 +18,10 @@ if(mdExists(ruta) == true){
   const rutaAbsoluta = path.resolve(ruta) // ruta resuelta
   mdValid(rutaAbsoluta)
   //console.log('Ok', mdValid(rutaAbsoluta));
+  //convertir a sincrona par asi dejar menos then
   mdValid(rutaAbsoluta)
   //si la promeda se resuelve se ejecuta then
+  //aqui debo ingresar mi mdread
   .then((response)=>{
     console.log(response);
   })
@@ -28,7 +30,7 @@ if(mdExists(ruta) == true){
   })
 }else{
     //si el archivo no existe, se imprime fail en la consola
-  console.log('Fail');
+  console.log('falladisimo');
 }
 // importar fn mdread
 // en la linea 19 llmar a la fn mdRead y enviar rutaAbsoluta
